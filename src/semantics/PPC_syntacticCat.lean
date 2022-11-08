54de2edf5f0b38145f0da7f192c0fc1542b6c2b9
Jacob Neumann <jacobneu@andrew.cmu.edu>
2022-11-08 17:07:56 +0000

    Cleaned up quotient use in syntactic poset construction

--- src/semantics/PPC_syntacticCat.lean
+++ src/semantics/PPC_syntacticCat.lean
@@ -1,10 +1,13 @@
 import languages.PPC proof.PPC_natDeduct category_theory.category.preorder
 
+-- ⊢ is a preorder on the set of formulas 
 instance PPC_preorder : preorder PPC_form :=
 { le := λ φ ψ, φ ⊢ ψ,
   le_refl := derive_refl,
   le_trans := derive_trans,
 }
+
+-- Inter-derivability is an equivalence relation on formulas
 instance PPC_setoid : setoid PPC_form :=
 { r := λ φ ψ, φ ⊢ ψ ∧ ψ ⊢ φ, 
   iseqv := ⟨ begin 
@@ -25,8 +28,10 @@ instance PPC_setoid : setoid PPC_form :=
 } 
 notation (name:=PPC_setoid.r) φ `⊣⊢` ψ :78 := PPC_setoid.r φ ψ 
 
-def PPC_equiv_class : Type := quot (⊣⊢) 
+-- Quotienting PPC_form by inter-derivability
+def PPC_eq : Type := quot (⊣⊢) 
 
+-- Lemmas that ⊢ respects ⊣⊢
 lemma PPC_preorder_liftable1 :
   ∀ φ ψ θ : PPC_form, ψ ⊣⊢ θ → (φ ⊢ ψ) = (φ ⊢ θ) :=
 begin 
@@ -47,56 +52,59 @@ begin
   assume φθ, apply derive_trans, exact ψφ, exact φθ,
   assume ψθ, apply derive_trans, exact φψ, exact ψθ,
 end 
-
-def PPC_preorder_lifted : PPC_equiv_class → PPC_equiv_class → Prop 
+#check quotient.sound
+-- Preorder on equivalence classes
+def PPC_eq_order   : PPC_eq → PPC_eq → Prop 
   := quot.lift₂ PPC_preorder.le PPC_preorder_liftable1 PPC_preorder_liftable2
-def preorder_lift_rewrite (φ ψ : PPC_form) 
-  : PPC_preorder_lifted ⟦φ⟧ ⟦ψ⟧ = (φ ⊢ ψ) 
+-- := begin
+--       assume a b,
+--       induction a with φ ψ₀ ψ₁ ψ₂,
+--       induction b with ψ φ₀ φ₁ φ₂,
+--       exact φ ⊢ ψ,
+--       -- proof that this respects ⊣⊢ in φ
+--       -- simp[eq.rec],
+--       apply propext,
+--       dsimp[setoid.r] at φ₂, cases φ₂ with forw rev,
+--       constructor, assume φφ₀, exact φφ₀, 
+--       --rewrite at φφ₀,
+--       apply derive_trans, exact φφ₀,  
+--    end
+
+/- 
+  Helpful lemma to allow us to use the natural deduction calculus
+  when working on equivalence classes of formulas
+-/
+lemma preorder_lift_rewrite (φ ψ : PPC_form) 
+  : PPC_eq_order ⟦φ⟧ ⟦ψ⟧ = (φ ⊢ ψ) 
   := begin refl end
 
-def quotient_monotone : ∀ φ ψ : PPC_form, 
-      φ ⊢ ψ → PPC_preorder_lifted ⟦φ⟧ ⟦ψ⟧ :=
-begin 
-  assume φ ψ h,
-  dsimp[PPC_preorder_lifted],
-  exact h,
-end 
-lemma quotient_universal : ∀ φ ψ : PPC_form,
-  PPC_preorder_lifted ⟦φ⟧ ⟦ψ⟧ → φ ⊢ ψ :=
-begin 
-  assume φ ψ h,
-  rewrite [preorder_lift_rewrite] at h,
-  exact h,
-end 
-
-instance PPC_poset : partial_order (PPC_equiv_class) :=
-{ le := PPC_preorder_lifted,
-  le_refl := (λ a, quotient.induction_on a
+--  PPC_form / ⊣⊢ is a poset under PPC_eq_order
+instance PPC_poset : partial_order (PPC_eq) :=
+{ le := PPC_eq_order,
+  le_refl := 
                 (begin
-                  assume φ,
-                  dsimp[(≤)],
-                  rewrite[preorder_lift_rewrite],
-                  apply derive_refl,
-                end)),
-  le_trans := (λ a b c, quotient.induction_on₃ a b c 
+                  assume a,
+                  induction a with φ, 
+                  dsimp[(≤),setoid.r,PPC_eq_order],
+                  apply derive_refl,refl,
+                end),
+  le_trans := 
                 (begin 
-                  assume φ ψ θ h j,
-                  dsimp[(≤),⟦_⟧], dsimp[(≤)] at h, dsimp[(≤)] at j,
-                  rewrite[preorder_lift_rewrite],
-                  rewrite[preorder_lift_rewrite] at h,
-                  rewrite[preorder_lift_rewrite] at j,
+                  assume a b c h j,
+                  induction a with φ, induction b with ψ, induction c with θ,
+                  dsimp[(≤),setoid.r,PPC_eq_order],
+                  dsimp[(≤)] at h, dsimp[(≤)] at j, 
                   apply derive_trans,
                   exact h, exact j,
-                end)),
-  le_antisymm := (λ a b, 
+                  refl, refl, refl,
+                end),
+  le_antisymm := ( 
                 begin
-                  apply (quotient.induction_on₂ a b),
-                  assume φ ψ h j,
+                  assume a b h j,
+                  induction a with φ, induction b with ψ,
                   apply quotient.sound,
                   dsimp[(≈),setoid.r],dsimp[(≤)] at h, dsimp[(≤)] at j,
-                  rewrite[preorder_lift_rewrite] at h,
-                  rewrite[preorder_lift_rewrite] at j,
-                  constructor, exact h, exact j,
+                  constructor, exact h, exact j, refl, refl,
                 end)
 }
 
