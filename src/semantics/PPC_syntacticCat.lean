1b44f0de4393f8ab782484e5c807867cec4aad71
Jacob Neumann <jacobneu@andrew.cmu.edu>
2022-11-16 09:46:23 +0000

    Automated the proofs that C_PPC is a CCC using tactic combinators

--- src/semantics/PPC_syntacticCat.lean
+++ src/semantics/PPC_syntacticCat.lean
@@ -29,6 +29,10 @@ begin
   apply hom_of_le_le_of_hom,
 end 
 
+def derives_of_hom {φ ψ : PPC_form} (f : ℂ_PPC_obj φ ⟶ ℂ_PPC_obj ψ): φ ⊢ ψ :=
+  Exists.fst(ℂ_PPC_full f)
+
+
 -- ℂ_PPC is a thin category
 theorem ℂ_PPC_thin : ∀ {X Y : PPC_eq} {f g : X ⟶ Y}, f = g 
 :=
@@ -41,100 +45,70 @@ begin
   refl,refl,
 end 
 
+/-
+Tactic for automatically proving that operations defined by induction
+on PPC_eq respect ⊣⊢, because ℂ_PPC is thin and therefore any
+two parallel arrows are equal
+-/ 
+meta def by_ℂ_thin : tactic unit :=
+`[ repeat{assume _}, repeat{ repeat{ apply funext, assume _},apply ℂ_PPC_thin }]
+
+meta def repeat_assume_PPC_eq_induct : tactic unit :=
+`[ 
+  assume X : PPC_eq,
+  try {repeat_assume_PPC_eq_induct},
+  induction X with φ
+  ]
+/-
+Tactic for doing constructions in ℂ_PPC which are actually just
+the derivation rules of the natural deduction calculus lifted onto
+equivalence classes
+Input should be of the form `[ apply derives.XXX ]
+-/
+meta def lift_derive_ℂ_PPC ( T : tactic unit) : tactic unit :=
+`[ 
+   -- Assume all the ℂ_PPC objects, and write them as ⦃φ⦄ for some φ
+   repeat_assume_PPC_eq_induct,
+   -- Introduce assumed morphisms
+   repeat {assume Y},
+   -- Change the goal to be about constructing a derivation instead of a
+   -- ℂ_PPC morphism
+   apply ℂ_PPC_hom,
+   -- Apply the input tactic, proving the derivation
+   T, 
+   -- Clean up
+   repeat {apply derives_x.refl},
+   repeat {apply derives_of_hom, assumption},
+   -- Prove this coherent w.r.t the ⊣⊢equiv relation, using thinness of ℂ_PPC 
+   by_ℂ_thin ]
+
 open specialCats
 
 -- ℂ_PPC has finite products
 instance : FP_cat PPC_eq :=
 {
   unit := ℂ_PPC_obj PPC_form.top,
-  term := 
-    begin
-      -- Actual construction
-      assume X,
-      induction X with φ,
-      change (quot.mk setoid.r φ) with (ℂ_PPC_obj φ),
-      apply ℂ_PPC_hom,
-      exact derives.truth,
-      -- Proving this respects ⊣⊢
-      apply ℂ_PPC_thin,
-    end,
+  term := by lift_derive_ℂ_PPC `[ apply derives.truth ],
   unit_η := λ X f, ℂ_PPC_thin,
   prod := (&⁼),
-  pr1 :=
-    begin
-      -- Actual construction
-      assume X Y,
-      induction X with φ, induction Y with ψ,
-      apply ℂ_PPC_hom,
-      apply derives.and_eliml,
-      apply derive_refl,
-      -- Proving this respects ⊣⊢
-      apply ℂ_PPC_thin,
-      apply ℂ_PPC_thin,
-    end,
-  pr2 :=
-    begin
-      -- Actual construction
-      assume X Y,
-      induction X with φ, induction Y with ψ,
-      apply ℂ_PPC_hom,
-      apply derives.and_elimr,
-      apply derive_refl,
-      -- Proving this respects ⊣⊢
-      apply ℂ_PPC_thin,
-      apply ℂ_PPC_thin,
-    end,
-  pair :=
-    begin
-      -- Actual construction
-      assume X Y Z f g,
-      induction X with φ, induction Y with ψ, induction Z with χ,
-      let h : χ ⊢ φ := le_of_hom f,
-      let h' : χ ⊢ ψ := le_of_hom g,
-      apply ℂ_PPC_hom,
-      apply derives.and_intro,
-      exact h, exact h',
-      -- Proving this respects ⊣⊢
-      apply funext, assume _, apply funext, assume _, apply ℂ_PPC_thin,
-      apply funext, assume _, apply ℂ_PPC_thin,
-      apply funext, assume _, apply ℂ_PPC_thin,
-    end,
+  pr1 := by lift_derive_ℂ_PPC `[ apply derives.and_eliml ],
+  pr2 := by lift_derive_ℂ_PPC `[ apply derives.and_elimr ],
+  pair := by lift_derive_ℂ_PPC `[ apply derives.and_intro ],
   prod_β1 := λ X Y Z f g, ℂ_PPC_thin,
   prod_β2 := λ X Y Z f g, ℂ_PPC_thin,
   prod_η :=  λ X Y, ℂ_PPC_thin,
 }
 
+-- ℂ_PPC is a cartesian closed category
 instance : CCC PPC_eq := 
 {
   exp := (⊃⁼),
-  eval := 
-    begin 
-      -- Actual construction
-      assume Y Z,
-      induction Y with ψ, induction Z with θ,
-      apply ℂ_PPC_hom,
-      apply union_Hyp_and,
-      apply modus_ponens,
-      -- Proving this respects ⊣⊢
-      apply ℂ_PPC_thin,
-      apply ℂ_PPC_thin,
-    end,
-  curry := 
-    begin 
-      -- Actual construction
-      assume X Y Z u,
-      induction X with φ, induction Y with ψ, induction Z with θ,
-      apply ℂ_PPC_hom,
-      have h : (φ&ψ) ⊢ θ,
-      exact (Exists.fst (ℂ_PPC_full u)),
-      apply derives.impl_intro,
-      apply and_Hyp_union,
-      exact h,
-      -- Proving this respects ⊣⊢
-      apply funext, assume _, apply ℂ_PPC_thin,
-      apply funext, assume _, apply ℂ_PPC_thin,
-      apply funext, assume _, apply ℂ_PPC_thin,
-    end,
+  eval := by lift_derive_ℂ_PPC `[ 
+                apply derives_x.union_Hyp_and,
+                apply derives_x.modus_ponens ],
+  curry := by lift_derive_ℂ_PPC `[ 
+                apply derives.impl_intro,
+                apply derives_x.and_Hyp_union ],
   curry_β := λ {X Y Z} u, ℂ_PPC_thin,
   curry_η := λ {X Y Z} v, ℂ_PPC_thin,
 }
