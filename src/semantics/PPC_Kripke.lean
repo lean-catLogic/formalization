9a38e64ebbf0cea45147ffc7f7abcbec0f7e26f2
Jacob Neumann <jacobneu@andrew.cmu.edu>
2023-02-18 23:19:34 +0000

    Updated PPC_Kripke to match new modular approach, adding argument to has_forcing for type of formulas. TODO: move the has_forcing namespace into its own file

--- src/semantics/PPC_Kripke.lean
+++ src/semantics/PPC_Kripke.lean
@@ -1,45 +1,47 @@
-import languages.PPC proof.PPC_natDeduct category_theory.category.preorder
+import languages.PPC_natDeduct category_theory.category.preorder
 
 namespace forcing_notation
-class has_forcing (X : Type) :=
-  (force : X → PPC_form → Prop)
+class has_forcing (X : Type) (Form : Type) :=
+  (force : X → Form → Prop)
 
 notation (name := force) k `⊩` φ := has_forcing.force k φ
 end forcing_notation
 
 
-namespace KripkeModels
+namespace PPC_KripkeModels
 
-class KripkeModel (K : Type) extends partial_order K :=
-  (forces_p : ℕ → K → Prop)
-  (down_closed : ∀ (n : ℕ) (j k : K), j ≤ k → forces_p n k → forces_p n j)
+  open PPC_defn
 
-def forces {K : Type} [KripkeModel K] : K → PPC_form → Prop
-| k ⊤ := true
-| k (p i) := KripkeModel.forces_p i k
-| k (φ & ψ) := (forces k φ) ∧ (forces k ψ)
-| k (φ ⊃ ψ) := ∀ j : K, j ≤ k → forces j φ → forces j ψ
+  class KripkeModel (K : Type) extends partial_order K :=
+    (forces_p : ℕ → K → Prop)
+    (down_closed : ∀ (n : ℕ) (j k : K), j ≤ k → forces_p n k → forces_p n j)
 
-open forcing_notation
--- Use ⊩ notation between points of a Kripke model and formulas
-instance {K : Type} [KripkeModel K] : has_forcing K := ⟨ forces ⟩ 
+  def forces {K : Type} [KripkeModel K] : K → PPC_Form → Prop
+  | k ⊤ := true
+  | k (p i) := KripkeModel.forces_p i k
+  | k (φ & ψ) := (forces k φ) ∧ (forces k ψ)
+  | k (φ ⊃ ψ) := ∀ j : K, j ≤ k → forces j φ → forces j ψ
 
--- Use ⊩ notation to say the whole model forces φ
-instance {K : Type} : has_forcing (KripkeModel K) :=
-{
-  force := λ K_struct φ, ∀ k : K, @forces K K_struct k φ
-}
+  open forcing_notation
+  -- Use ⊩ notation between points of a Kripke model and formulas
+  instance pointwiseForcing {K : Type} [KripkeModel K] : has_forcing K PPC_Form := ⟨ forces ⟩ 
 
--- Use ()⊩φ to say that every model forces φ
-instance : has_forcing unit :=
-{
-  force := λ _ φ, ∀ (K : Type) (K_struct : KripkeModel K), K_struct ⊩ φ
-}
+  -- Use ⊩ notation to say the whole model forces φ
+  instance globalForcing {K : Type} : has_forcing (KripkeModel K) PPC_Form :=
+  {
+    force := λ K_struct φ, ∀ k : K, @forces K K_struct k φ
+  }
 
-lemma top_valid : () ⊩ ⊤ :=
-begin 
-  assume K K_struct k,
-  constructor,
-end 
+  -- Use ()⊩φ to say that every model forces φ
+  instance universalForcing : has_forcing unit PPC_Form :=
+  {
+    force := λ _ φ, ∀ (K : Type) (K_struct : KripkeModel K), K_struct ⊩ φ
+  }
 
-end KripkeModels
+  lemma top_valid : () ⊩ PPC_Form.top :=
+  begin 
+    assume K K_struct k,
+    constructor,
+  end 
+
+end PPC_KripkeModels
