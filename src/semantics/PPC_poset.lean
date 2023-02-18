162a055a49df9ae1177e64c796a230110f22224c
Jacob Neumann <jacobneu@andrew.cmu.edu>
2023-02-18 14:25:01 +0000

    Abstracted syntactic poset construction to arbitrary has_derives, reimplemented PPC as instance

--- src/semantics/PPC_poset.lean
+++ src/semantics/PPC_poset.lean
@@ -1,140 +1,48 @@
-import languages.PPC proof.PPC_natDeduct category_theory.category.preorder
+import semantics.syntacticPoset languages.PPC_natDeduct
 
--- ⊢ is a preorder on the set of formulas 
-instance PPC_preorder : preorder PPC_form :=
-{ le := λ φ ψ, φ ⊢ ψ,
-  le_refl := derives_x.refl,
-  le_trans := derives_x.trans,
-}
+namespace PPC_synPoset
 
--- Inter-derivability is an equivalence relation on formulas
-instance PPC_setoid : setoid PPC_form :=
-{ r := λ φ ψ, φ ⊢ ψ ∧ ψ ⊢ φ, 
-  iseqv := ⟨ begin 
-              assume φ, constructor, 
-              apply derives_x.refl, apply derives_x.refl 
-             end, 
-            begin
-              assume φ ψ h, cases h with φψ ψφ,
-              constructor, exact ψφ, exact φψ,
-            end,
-            begin 
-              assume φ ψ θ j k, cases j with φψ ψφ,
-              cases k with ψθ θψ, constructor,
-              apply derives_x.trans, exact φψ, exact ψθ,
-              apply derives_x.trans, exact θψ, exact ψφ,
-            end 
-            ⟩ 
-} 
-notation (name:=PPC_setoid.r) φ `⊣⊢` ψ :78 := PPC_setoid.r φ ψ 
+open PPC_defn
+open PPC_defn.PPC_derives
+open PPC_has_derives
+open synacticPoset
 
--- Quotienting PPC_form by inter-derivability
-def PPC_eq : Type := quot (⊣⊢) 
+def PPC_Der := PPC_has_derives.PPC_Der
+def derive_refl := PPC_Der.derive_refl
+def derive_trans := PPC_Der.derive_trans
 
-def PPC_eq_in (φ : PPC_form) : PPC_eq := ⟦φ⟧
+def PPC_eq := @Form_eq PPC_Form PPC_Der
 
-notation (name:=PPC_eq_in) ⦃φ⦄ := PPC_eq_in φ
-
--- Lemmas that ⊢ respects ⊣⊢
-lemma PPC_preorder_liftable1 :
-  ∀ φ ψ θ : PPC_form, ψ ⊣⊢ θ → (φ ⊢ ψ) = (φ ⊢ θ) :=
-begin 
-  assume φ ψ θ ψiffθ,
-  cases ψiffθ with ψθ θψ,
-  apply propext,
-  constructor,
-  assume φψ, apply derives_x.trans, exact φψ, exact ψθ,
-  assume φθ, apply derives_x.trans, exact φθ, exact θψ,
-end
-lemma PPC_preorder_liftable2 :
-  ∀ φ ψ θ : PPC_form, φ ⊣⊢ ψ → (φ ⊢ θ) = (ψ ⊢ θ) :=
-begin 
-  assume φ ψ θ φiffψ,
-  cases φiffψ with φψ ψφ,
-  apply propext,
-  constructor,
-  assume φθ, apply derives_x.trans, exact ψφ, exact φθ,
-  assume ψθ, apply derives_x.trans, exact φψ, exact ψθ,
-end 
-
-
--- Preorder on equivalence classes
-def PPC_eq_order   : PPC_eq → PPC_eq → Prop 
-  := quot.lift₂ PPC_preorder.le PPC_preorder_liftable1 PPC_preorder_liftable2
-
-
-
-/- 
-  Helpful lemma to allow us to use the natural deduction calculus
-  when working on equivalence classes of formulas
--/
-lemma preorder_lift_rewrite (φ ψ : PPC_form) 
-  : PPC_eq_order ⦃φ⦄ ⦃ψ⦄ = (φ ⊢ ψ) 
-  := begin refl end
-
---  PPC_form / ⊣⊢ is a poset under PPC_eq_order
-instance PPC_poset : partial_order (PPC_eq) :=
-{ le := PPC_eq_order,
-  le_refl := 
-    begin
-      assume a,
-      induction a with φ, 
-      dsimp[(≤),setoid.r,PPC_eq_order],
-      apply derives_x.refl,refl,
-    end,
-  le_trans := 
-    begin 
-      assume a b c h j,
-      induction a with φ, induction b with ψ, induction c with θ,
-      dsimp[(≤),setoid.r,PPC_eq_order],
-      dsimp[(≤)] at h, dsimp[(≤)] at j, 
-      apply derives_x.trans,
-      exact h, exact j,
-      refl, refl, refl,
-    end,
-  le_antisymm :=  
-    begin
-      assume a b h j,
-      induction a with φ, induction b with ψ,
-      apply quotient.sound,
-      dsimp[(≈),setoid.r],dsimp[(≤)] at h, dsimp[(≤)] at j,
-      constructor, exact h, exact j, refl, refl,
-    end
-}
-
-instance PPC_eq_pre : preorder PPC_eq :=
-  @partial_order.to_preorder PPC_eq PPC_poset
-
-lemma and_liftable1 : ∀ φ ψ ψ' : PPC_form, 
+lemma and_liftable1 : ∀ φ ψ ψ' : PPC_Form, 
   (ψ ⊣⊢ ψ') → ⦃φ & ψ⦄ = ⦃φ & ψ'⦄ :=
-begin
+  begin
     assume φ ψ ψ' h,
     apply quotient.sound,
     cases h with ψψ' ψ'ψ,
     constructor,
     -- Proof that φ&ψ ⊢ φ&ψ'
-      apply derives.and_intro,
+      apply and_intro,
       -- φ&ψ ⊢ φ
-      apply @derives.and_eliml {φ&ψ} φ ψ,
-      apply derives_x.refl, -- φ&ψ ⊢ φ&ψ
+      apply and_eliml,
+      apply derive_refl, -- φ&ψ ⊢ φ&ψ
       -- φ&ψ ⊢ ψ'
-      apply derives_x.trans,
-      apply @derives.and_elimr {φ&ψ} φ ψ,
-      apply derives_x.refl, -- φ&ψ ⊢ φ&ψ
+      apply derive_trans,
+      apply and_elimr,
+      apply derive_refl, -- φ&ψ ⊢ φ&ψ
       exact ψψ',
     -- Proof that φ&ψ' ⊢ φ&ψ
-      apply derives.and_intro,
+      apply and_intro,
       -- φ&ψ ⊢ φ
-      apply @derives.and_eliml {φ&ψ'} φ ψ',
-      apply derives_x.refl, -- φ&ψ' ⊢ φ&ψ'
+      apply and_eliml,
+      apply derive_refl, -- φ&ψ' ⊢ φ&ψ'
       -- φ&ψ' ⊢ ψ
-      apply derives_x.trans,
-      apply @derives.and_elimr {φ&ψ'} φ ψ',
-      apply derives_x.refl, -- φ&ψ' ⊢ φ&ψ'
+      apply derive_trans,
+      apply and_elimr,
+      apply derive_refl, -- φ&ψ' ⊢ φ&ψ'
       exact ψ'ψ,
 end
 
-lemma and_liftable2 : ∀ φ φ' ψ : PPC_form, 
+lemma and_liftable2 : ∀ φ φ' ψ : PPC_Form, 
   (φ ⊣⊢ φ') → ⦃φ & ψ⦄ = ⦃φ' & ψ⦄ := 
 begin
     assume φ φ' ψ h,
@@ -142,64 +50,64 @@ begin
     cases h with φφ' φ'φ,
     constructor,
     -- Proof that φ&ψ ⊢ φ'&ψ
-      apply derives.and_intro,
-      apply derives_x.trans,
-      apply @derives.and_eliml {φ&ψ} φ ψ,
-      apply derives_x.refl, -- φ&ψ ⊢ φ&ψ
+      apply and_intro,
+      apply derive_trans,
+      apply and_eliml,
+      apply derive_refl, -- φ&ψ ⊢ φ&ψ
       exact φφ',
-      apply @derives.and_elimr {φ&ψ} φ ψ,
-      apply derives_x.refl, -- φ&ψ ⊢ φ&ψ
+      apply and_elimr,
+      apply derive_refl, -- φ&ψ ⊢ φ&ψ
     -- Proof that φ'&ψ ⊢ φ&ψ
-      apply derives.and_intro,
-      apply derives_x.trans,
-      apply @derives.and_eliml {φ'&ψ} φ' ψ,
-      apply derives_x.refl, -- φ'&ψ ⊢ φ'&ψ
+      apply and_intro,
+      apply derive_trans,
+      apply and_eliml,
+      apply derive_refl, -- φ'&ψ ⊢ φ'&ψ
       exact φ'φ,
-      apply @derives.and_elimr {φ'&ψ} φ' ψ,
-      apply derives_x.refl, -- φ'&ψ ⊢ φ'&ψ
+      apply and_elimr,
+      apply derive_refl, -- φ'&ψ ⊢ φ'&ψ
 end
 
-def and_eq : PPC_eq → PPC_eq → PPC_eq :=
+def and_eq : Form_eq → Form_eq → Form_eq :=
   quot.lift₂ (λ φ ψ, ⦃φ & ψ⦄) and_liftable1 and_liftable2 
 
 notation (name := and_eq) X `&⁼` Y := and_eq X Y
 
-lemma impl_liftable1 : ∀ φ ψ ψ' : PPC_form, 
+lemma impl_liftable1 : ∀ φ ψ ψ' : PPC_Form, 
   (ψ ⊣⊢ ψ') → ⦃φ ⊃ ψ⦄ = ⦃φ ⊃ ψ'⦄ :=
 begin 
     assume φ ψ ψ' h,
     apply quotient.sound,
     cases h with ψψ' ψ'ψ,
     constructor,
-      apply derives.impl_intro,
-      apply derives_x.trans_hyp,
-      exact (derives_x.modus_ponens φ ψ),
+      apply impl_intro,
+      apply PPC_derives_x.trans_hyp,
+      rw PPC_defn.insert_is_union_singleton,
+      apply PPC_derives_x.modus_ponens,
       exact ψψ',
-      apply derives.impl_intro,
-      apply derives_x.trans_hyp,
-      exact (derives_x.modus_ponens φ ψ'),
+      apply impl_intro,
+      apply PPC_derives_x.trans_hyp,
+      rw PPC_defn.insert_is_union_singleton,
+      apply PPC_derives_x.modus_ponens,
       exact ψ'ψ,
 end 
 
-lemma impl_liftable2 : ∀ φ φ' ψ : PPC_form, 
+lemma impl_liftable2 : ∀ φ φ' ψ : PPC_Form, 
   (φ ⊣⊢ φ') → ⦃φ ⊃ ψ⦄ = ⦃φ' ⊃ ψ⦄ := 
 begin 
     assume φ φ' ψ h,
     apply quotient.sound,
     cases h with φφ' φ'φ,
     constructor,
-      apply derives.impl_elim,
-      apply derives_x.hypo_syll',
-      apply derives.impl_intro,
-      rewrite Hyp_x.just_flip,
-      apply derives.weak,
+      apply impl_elim,
+      apply PPC_derives_x.hypo_syll',
+      apply impl_intro,
+      apply weak,
       exact φ'φ,
       ---
-      apply derives.impl_elim,
-      apply derives_x.hypo_syll',
-      apply derives.impl_intro,
-      rewrite Hyp_x.just_flip,
-      apply derives.weak,
+      apply impl_elim,
+      apply PPC_derives_x.hypo_syll',
+      apply impl_intro,
+      apply weak,
       exact φφ',
 end 
 
@@ -209,3 +117,4 @@ def impl_eq : PPC_eq → PPC_eq → PPC_eq :=
 
 notation (name := impl_eq) X `⊃⁼` Y := impl_eq X Y
 
+end PPC_synPoset
---
+++ src/semantics/syntacticPoset.lean
@@ -0,0 +1,107 @@
+import languages.deductive category_theory.category.preorder
+
+namespace synacticPoset
+
+variable {Form : Type}
+-- variable {Der : has_derives Form}
+
+
+instance syn_preorder [Der : has_derives Form] : preorder Form :=
+{ le := λ φ ψ, φ ⊢ ψ,
+  le_refl := Der.derive_refl,
+  le_trans := Der.derive_trans,
+}
+
+def inter_der [Der : has_derives Form] : Form → Form → Prop :=
+  λ φ ψ,  φ ⊢ ψ ∧ ψ ⊢ φ
+
+infix `⊣⊢`:78 := inter_der
+
+instance syn_setoid [Der : has_derives Form] : setoid Form :=
+{ r := (⊣⊢), 
+  iseqv := ⟨ begin 
+              assume φ, constructor, 
+              apply Der.derive_refl, apply Der.derive_refl,
+             end, 
+            begin
+              assume φ ψ h, cases h with φψ ψφ,
+              constructor, exact ψφ, exact φψ,
+            end,
+            begin 
+              assume φ ψ θ j k, cases j with φψ ψφ,
+              cases k with ψθ θψ, constructor,
+              apply Der.derive_trans, exact φψ, exact ψθ,
+              apply Der.derive_trans, exact θψ, exact ψφ,
+            end 
+            ⟩ 
+}
+
+-- notation (name:=syn_setoid.r) φ `⊣⊢` ψ :78 := syn_setoid.r φ ψ 
+
+def Form_eq [Der : has_derives Form] : Type := @quot Form (⊣⊢)
+
+def Form_eq_in [Der : has_derives Form] : Form → @Form_eq Form Der := quot.mk (⊣⊢) 
+
+notation (name:=PPC_eq_in) ⦃φ⦄ := Form_eq_in φ
+
+-- Lemmas that ⊢ respects ⊣⊢
+lemma syn_preorder_liftable1 [Der : has_derives Form] :
+  ∀ φ ψ θ : Form, ψ ⊣⊢ θ → (φ ⊢ ψ) = (φ ⊢ θ) :=
+begin 
+  assume φ ψ θ ψiffθ,
+  cases ψiffθ with ψθ θψ,
+  apply propext,
+  constructor,
+  assume φψ, apply Der.derive_trans, exact φψ, exact ψθ,
+  assume φθ, apply Der.derive_trans, exact φθ, exact θψ,
+end
+lemma syn_preorder_liftable2 [Der : has_derives Form] :
+  ∀ φ ψ θ : Form, φ ⊣⊢ ψ → (φ ⊢ θ) = (ψ ⊢ θ) :=
+begin 
+  assume φ ψ θ φiffψ,
+  cases φiffψ with φψ ψφ,
+  apply propext,
+  constructor,
+  assume φθ, apply Der.derive_trans, exact ψφ, exact φθ,
+  assume ψθ, apply Der.derive_trans, exact φψ, exact ψθ,
+end 
+
+def Form_eq_order [Der : has_derives Form] : @Form_eq Form Der → @Form_eq Form Der → Prop 
+  := quot.lift₂ synacticPoset.syn_preorder.le syn_preorder_liftable1 syn_preorder_liftable2
+
+
+
+instance syn_poset [Der : has_derives Form] : partial_order Form_eq :=
+{ le := @Form_eq_order Form Der,
+  le_refl := 
+    begin
+      assume a,
+      induction a with φ, 
+      dsimp[(≤),setoid.r,Form_eq_order],
+      apply Der.derive_refl,refl,
+    end,
+  le_trans := 
+    begin 
+      assume a b c h j,
+      induction a with φ, induction b with ψ, induction c with θ,
+      dsimp[(≤),setoid.r,Form_eq_order],
+      dsimp[(≤)] at h, dsimp[(≤)] at j, 
+      apply Der.derive_trans,
+      exact h, exact j,
+      refl, refl, refl,
+    end,
+  le_antisymm :=  
+    begin
+      assume a b h j,
+      induction a with φ, induction b with ψ,
+      apply quotient.sound,
+      dsimp[(≈),setoid.r],dsimp[(≤)] at h, dsimp[(≤)] at j,
+      constructor, exact h, exact j, refl, refl,
+    end
+}
+
+end synacticPoset
+
+
+-- instance syn_eq_pre [Der : has_derives Form] : preorder (@Form_eq Form Der) :=
+--   @partial_order.to_preorder Form_eq syntacticPoset.syn_poset
