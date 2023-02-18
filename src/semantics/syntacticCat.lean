cf483d4625293a2ce688522b2338bf4e388586f0
Jacob Neumann <jacobneu@andrew.cmu.edu>
2023-02-18 23:09:38 +0000

    Finished PPC_natDeduct lemmas to get PPC_syntacticCat to go through. Some cleanup

--- src/semantics/syntacticCat.lean
+++ src/semantics/syntacticCat.lean
@@ -2,28 +2,27 @@ import semantics.syntacticPoset CT.CCC category_theory.category.preorder
 open category_theory
 
 namespace synCat
-open synPoset
+  open synPoset
 
-variable {Form : Type}
+  variable {Form : Type}
 
-#check synPoset.syn_eq_pre
--- Form the category out of this poset
-def syn_cat [Der : has_derives Form] : thin_cat (Form _eq)
-  := thin_cat.from_preorder synPoset.syn_eq_pre
-instance [Der : has_derives Form] : thin_cat (Form _eq) := syn_cat
+  -- Form the category out of this poset
+  def syn_cat [Der : has_derives Form] : thin_cat (Form _eq)
+    := thin_cat.from_preorder synPoset.syn_eq_pre
+  instance [Der : has_derives Form] : thin_cat (Form _eq) := syn_cat
 
--- Methods for directly turning formulas into objects and derivations into morphisms
-def syn_obj [Der : has_derives Form] (φ : Form) : Form _eq := Form_eq_in φ
-def syn_hom [Der : has_derives Form] : ∀ {φ ψ : Form}, (φ ⊢ ψ) → (syn_obj φ ⟶ syn_obj ψ) 
-:= begin
-  assume φ ψ h,
-  apply hom_of_le,
-  exact h,
-end 
+  -- Methods for directly turning formulas into objects and derivations into morphisms
+  def syn_obj [Der : has_derives Form] (φ : Form) : Form _eq := Form_eq_in φ
+  def syn_hom [Der : has_derives Form] : ∀ {φ ψ : Form}, (φ ⊢ ψ) → (syn_obj φ ⟶ syn_obj ψ) 
+  := begin
+    assume φ ψ h,
+    apply hom_of_le,
+    exact h,
+  end 
 
-def derives_of_hom [Der : has_derives Form] {φ ψ : Form} (f : syn_obj φ ⟶ syn_obj ψ): 
-  Der.derives (Der.singleHyp.singleton φ) ψ :=
-  Exists.fst(thin_cat.preorder_cat_full f)
+  def derives_of_hom [Der : has_derives Form] {φ ψ : Form} (f : syn_obj φ ⟶ syn_obj ψ): 
+    Der.derives (Der.singleHyp.singleton φ) ψ :=
+    Exists.fst(thin_cat.preorder_cat_full f)
 
 end synCat
 
@@ -31,56 +30,55 @@ end synCat
 
 namespace synCat_tactics
 
-/-
-  To instantiate these tactics for a specific deductive calculus,
-  create a namespace with Form and Der specifically def'd (with
-  those exact variable names), and then
+  /-
+    To instantiate these tactics for a specific deductive calculus,
+    create a namespace with Form and Der specifically def'd (with
+    those exact variable names), and then
 
-  meta def lift_derive_XXX : tactic unit → tactic unit :=
-  @synCat_tactics.lift_derive_syn_cat Form Der
--/
-variable {Form : Type}
-variable [Der : has_derives Form]
+    meta def lift_derive_XXX : tactic unit → tactic unit :=
+    @synCat_tactics.lift_derive_syn_cat Form Der
+  -/
+  variable {Form : Type}
+  variable [Der : has_derives Form]
 
-/-
-Tactic for automatically proving that operations defined by induction
-on Form_eq respect ⊣⊢, because syn_cat is thin and therefore any
-two parallel arrows are equal
--/ 
-meta def by_syn_thin {Form : Type} [Der : has_derives Form] : tactic unit :=
-`[ repeat{assume _}, repeat{ repeat{ apply funext, assume _},apply thin_cat.K }]
+  /-
+  Tactic for automatically proving that operations defined by induction
+  on Form_eq respect ⊣⊢, because syn_cat is thin and therefore any
+  two parallel arrows are equal
+  -/ 
+  meta def by_syn_thin {Form : Type} [Der : has_derives Form] : tactic unit :=
+  `[ repeat{assume _}, repeat{ repeat{ apply funext, assume _},apply thin_cat.K }]
 
-meta def repeat_assume_Form_eq_induct {Form : Type} [Der : has_derives Form] : tactic unit :=
-`[ 
-  assume X : @synPoset.Form_eq Form Der,
-  try {repeat_assume_Form_eq_induct},
-  induction X with φ
+  meta def repeat_assume_Form_eq_induct {Form : Type} [Der : has_derives Form] : tactic unit :=
+  `[ 
+    assume X : @synPoset.Form_eq Form Der,
+    try {repeat_assume_Form_eq_induct},
+    induction X with φ
   ]
-/-
-Tactic for doing constructions in syn_cat which are actually just
-the derivation rules lifted onto equivalence classes
-Input should be of the form `[ apply XXX ]
-where XXX is a derivation rule of Der
--/
-meta def lift_derive_syn_cat 
-  (T : tactic unit) : tactic unit :=
-  -- do
-  --   goal ← tactic.target,
-  --   tactic.trace goal
-`[ 
-   -- Assume all the ℂ_PPC objects, and write them as ⦃φ⦄ for some φ
-   @repeat_assume_Form_eq_induct Form Der,
-   -- Introduce assumed morphisms
-   repeat {assume Y},
-   -- Change the goal to be about constructing a derivation instead of a
-   -- ℂ_PPC morphism
-   apply syn_hom,
-   -- Apply the input tactic, proving the derivation
-   T, 
-   -- Clean up
-   repeat {apply PPC_Der.derive_refl},
-   repeat {apply @derives_of_hom Form Der, assumption},
-   -- Prove this coherent w.r.t the ⊣⊢equiv relation, using thinness of ℂ_PPC 
-   @by_syn_thin Form Der ]
+
+  /-
+  Tactic for doing constructions in syn_cat which are actually just
+  the derivation rules lifted onto equivalence classes
+  Input should be of the form `[ apply XXX ]
+  where XXX is a derivation rule of Der
+  -/
+  meta def lift_derive_syn_cat 
+    (T : tactic unit) : tactic unit :=
+  `[ 
+    -- Assume all the ℂ_PPC objects, and write them as ⦃φ⦄ for some φ
+    @repeat_assume_Form_eq_induct Form Der,
+    -- Introduce assumed morphisms
+    repeat {assume Y},
+    -- Change the goal to be about constructing a derivation instead of a
+    -- ℂ_PPC morphism
+    apply syn_hom,
+    -- Apply the input tactic, proving the derivation
+    T, 
+    -- Clean up
+    repeat {apply PPC_Der.derive_refl},
+    repeat {apply @derives_of_hom Form Der, assumption},
+    -- Prove this coherent w.r.t the ⊣⊢equiv relation, using thinness of ℂ_PPC 
+    @by_syn_thin Form Der 
+  ]
 
 end synCat_tactics
--- src/semantics/syntacticPoset.lean
+++ src/semantics/syntacticPoset.lean
@@ -2,109 +2,107 @@ import languages.deductive category_theory.category.preorder
 
 namespace synPoset
 
-variable {Form : Type}
--- variable {Der : has_derives Form}
+  variable {Form : Type}
 
+  instance syn_preorder [Der : has_derives Form] : preorder Form :=
+  { le := λ φ ψ, φ ⊢ ψ,
+    le_refl := Der.derive_refl,
+    le_trans := Der.derive_trans,
+  }
 
-instance syn_preorder [Der : has_derives Form] : preorder Form :=
-{ le := λ φ ψ, φ ⊢ ψ,
-  le_refl := Der.derive_refl,
-  le_trans := Der.derive_trans,
-}
+  def inter_der [Der : has_derives Form] : Form → Form → Prop :=
+    λ φ ψ,  φ ⊢ ψ ∧ ψ ⊢ φ
 
-def inter_der [Der : has_derives Form] : Form → Form → Prop :=
-  λ φ ψ,  φ ⊢ ψ ∧ ψ ⊢ φ
+  infix `⊣⊢`:78 := inter_der
 
-infix `⊣⊢`:78 := inter_der
+  instance syn_setoid [Der : has_derives Form] : setoid Form :=
+  { r := (⊣⊢), 
+    iseqv := ⟨ begin 
+                assume φ, constructor, 
+                apply Der.derive_refl, apply Der.derive_refl,
+              end, 
+              begin
+                assume φ ψ h, cases h with φψ ψφ,
+                constructor, exact ψφ, exact φψ,
+              end,
+              begin 
+                assume φ ψ θ j k, cases j with φψ ψφ,
+                cases k with ψθ θψ, constructor,
+                apply Der.derive_trans, exact φψ, exact ψθ,
+                apply Der.derive_trans, exact θψ, exact ψφ,
+              end 
+              ⟩ 
+  }
 
-instance syn_setoid [Der : has_derives Form] : setoid Form :=
-{ r := (⊣⊢), 
-  iseqv := ⟨ begin 
-              assume φ, constructor, 
-              apply Der.derive_refl, apply Der.derive_refl,
-             end, 
-            begin
-              assume φ ψ h, cases h with φψ ψφ,
-              constructor, exact ψφ, exact φψ,
-            end,
-            begin 
-              assume φ ψ θ j k, cases j with φψ ψφ,
-              cases k with ψθ θψ, constructor,
-              apply Der.derive_trans, exact φψ, exact ψθ,
-              apply Der.derive_trans, exact θψ, exact ψφ,
-            end 
-            ⟩ 
-}
+  -- notation (name:=syn_setoid.r) φ `⊣⊢` ψ :78 := syn_setoid.r φ ψ 
 
--- notation (name:=syn_setoid.r) φ `⊣⊢` ψ :78 := syn_setoid.r φ ψ 
+  def Form_eq [Der : has_derives Form] : Type := @quot Form (⊣⊢)
+  def Form_eq_x (F : Type) [Der : has_derives F] : Type := @Form_eq F Der
 
-def Form_eq [Der : has_derives Form] : Type := @quot Form (⊣⊢)
-def Form_eq_x (F : Type) [Der : has_derives F] : Type := @Form_eq F Der
+  notation (name:=Form_eq_explicit) F ` _eq`:max := Form_eq_x F
 
-notation (name:=Form_eq_explicit) F ` _eq`:max := Form_eq_x F
+  def Form_eq_in [Der : has_derives Form] : Form → @Form_eq Form Der := quot.mk (⊣⊢) 
 
-def Form_eq_in [Der : has_derives Form] : Form → @Form_eq Form Der := quot.mk (⊣⊢) 
+  notation (name:=PPC_eq_in) ⦃φ⦄ := Form_eq_in φ
 
-notation (name:=PPC_eq_in) ⦃φ⦄ := Form_eq_in φ
+  -- Lemmas that ⊢ respects ⊣⊢
+  lemma syn_preorder_liftable1 [Der : has_derives Form] :
+    ∀ φ ψ θ : Form, ψ ⊣⊢ θ → (φ ⊢ ψ) = (φ ⊢ θ) :=
+  begin 
+    assume φ ψ θ ψiffθ,
+    cases ψiffθ with ψθ θψ,
+    apply propext,
+    constructor,
+    assume φψ, apply Der.derive_trans, exact φψ, exact ψθ,
+    assume φθ, apply Der.derive_trans, exact φθ, exact θψ,
+  end
+  lemma syn_preorder_liftable2 [Der : has_derives Form] :
+    ∀ φ ψ θ : Form, φ ⊣⊢ ψ → (φ ⊢ θ) = (ψ ⊢ θ) :=
+  begin 
+    assume φ ψ θ φiffψ,
+    cases φiffψ with φψ ψφ,
+    apply propext,
+    constructor,
+    assume φθ, apply Der.derive_trans, exact ψφ, exact φθ,
+    assume ψθ, apply Der.derive_trans, exact φψ, exact ψθ,
+  end 
 
--- Lemmas that ⊢ respects ⊣⊢
-lemma syn_preorder_liftable1 [Der : has_derives Form] :
-  ∀ φ ψ θ : Form, ψ ⊣⊢ θ → (φ ⊢ ψ) = (φ ⊢ θ) :=
-begin 
-  assume φ ψ θ ψiffθ,
-  cases ψiffθ with ψθ θψ,
-  apply propext,
-  constructor,
-  assume φψ, apply Der.derive_trans, exact φψ, exact ψθ,
-  assume φθ, apply Der.derive_trans, exact φθ, exact θψ,
-end
-lemma syn_preorder_liftable2 [Der : has_derives Form] :
-  ∀ φ ψ θ : Form, φ ⊣⊢ ψ → (φ ⊢ θ) = (ψ ⊢ θ) :=
-begin 
-  assume φ ψ θ φiffψ,
-  cases φiffψ with φψ ψφ,
-  apply propext,
-  constructor,
-  assume φθ, apply Der.derive_trans, exact ψφ, exact φθ,
-  assume ψθ, apply Der.derive_trans, exact φψ, exact ψθ,
-end 
-
-def Form_eq_order [Der : has_derives Form] : @Form_eq Form Der → @Form_eq Form Der → Prop 
-  := quot.lift₂ synPoset.syn_preorder.le syn_preorder_liftable1 syn_preorder_liftable2
+  def Form_eq_order [Der : has_derives Form] : @Form_eq Form Der → @Form_eq Form Der → Prop 
+    := quot.lift₂ synPoset.syn_preorder.le syn_preorder_liftable1 syn_preorder_liftable2
 
 
 
-instance syn_poset [Der : has_derives Form] : partial_order Form_eq :=
-{ le := @Form_eq_order Form Der,
-  le_refl := 
-    begin
-      assume a,
-      induction a with φ, 
-      dsimp[(≤),setoid.r,Form_eq_order],
-      apply Der.derive_refl,refl,
-    end,
-  le_trans := 
-    begin 
-      assume a b c h j,
-      induction a with φ, induction b with ψ, induction c with θ,
-      dsimp[(≤),setoid.r,Form_eq_order],
-      dsimp[(≤)] at h, dsimp[(≤)] at j, 
-      apply Der.derive_trans,
-      exact h, exact j,
-      refl, refl, refl,
-    end,
-  le_antisymm :=  
-    begin
-      assume a b h j,
-      induction a with φ, induction b with ψ,
-      apply quotient.sound,
-      dsimp[(≈),setoid.r],dsimp[(≤)] at h, dsimp[(≤)] at j,
-      constructor, exact h, exact j, refl, refl,
-    end
-}
+  instance syn_poset [Der : has_derives Form] : partial_order Form_eq :=
+  { le := @Form_eq_order Form Der,
+    le_refl := 
+      begin
+        assume a,
+        induction a with φ, 
+        dsimp[(≤),setoid.r,Form_eq_order],
+        apply Der.derive_refl,refl,
+      end,
+    le_trans := 
+      begin 
+        assume a b c h j,
+        induction a with φ, induction b with ψ, induction c with θ,
+        dsimp[(≤),setoid.r,Form_eq_order],
+        dsimp[(≤)] at h, dsimp[(≤)] at j, 
+        apply Der.derive_trans,
+        exact h, exact j,
+        refl, refl, refl,
+      end,
+    le_antisymm :=  
+      begin
+        assume a b h j,
+        induction a with φ, induction b with ψ,
+        apply quotient.sound,
+        dsimp[(≈),setoid.r],dsimp[(≤)] at h, dsimp[(≤)] at j,
+        constructor, exact h, exact j, refl, refl,
+      end
+  }
 
-instance syn_eq_pre {Form : Type} [Der : has_derives Form] : preorder (Form_eq_x Form) :=
-  @partial_order.to_preorder (Form_eq_x Form) synPoset.syn_poset
+  instance syn_eq_pre {Form : Type} [Der : has_derives Form] : preorder (Form_eq_x Form) :=
+    @partial_order.to_preorder (Form_eq_x Form) synPoset.syn_poset
 
 end synPoset
 
