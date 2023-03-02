026eccdee447291865f809eb5f8a01ce12ebc1d4
Jacob Neumann <jacobneu@andrew.cmu.edu>
2023-03-02 16:49:23 +0000

    Moved general-purpose tactics from synPoset and synCat to their own top-level file. Documented better /w comments

--- src/semantics/syntacticCat.lean
+++ src/semantics/syntacticCat.lean
@@ -32,17 +32,15 @@ namespace synCat_tactics
 
   open tactic
   open nat
+
   /-
-    To instantiate these tactics for a specific deductive calculus,
-    create a namespace with Form and Der specifically def'd (with
-    those exact variable names), and then
-
-    meta def lift_derive_XXX : tactic unit → tactic unit :=
-    @synCat_tactics.lift_derive_syn_cat Form Der
+  Helper tactic for lift_derive_syn_cat. After the tactic argument to 
+  lift_derive_syn_cat has broken the derivation goal down, cleanup goes
+  through and applies derive_refl and/or {derives_of_hom; assumption} to
+  finish off the main goal. It then uses the thinness of the syntactic
+  category to prove that this construction must respect the ⊣⊢ equiv
+  relation. This should conclude the lifting proof.
   -/
-  variable {Form : Type}
-  variable [Der : has_derives Form]
-
   meta def cleanup {Form : Type} [Der : has_derives Form] : tactic unit :=
     `[ 
       repeat {
@@ -52,29 +50,30 @@ namespace synCat_tactics
       }, 
       thin_cat.by_thin ]
 
-  def varFormat (baseName : name) (i : nat) : name :=
-    name.append_suffix baseName (nat.has_repr.repr i)
-
-  def gen_nameList (baseName : name) (n : nat) : list name := 
-    list.map (varFormat baseName) (list.range n)
-
   /-
   Tactic for doing constructions in syn_cat which are actually just
   the derivation rules lifted onto equivalence classes
   First two arguments are the number of objects and morphisms to assume
   Tactic input should be of the form `[ apply XXX ]
   where XXX is a derivation rule of Der
+
+  To instantiate these tactics for a specific deductive calculus,
+  create a namespace with Form and Der specifically def'd (with
+  those exact variable names), and then
+
+  meta def lift_derive_XXX : tactic unit → tactic unit :=
+  @synCat_tactics.lift_derive_syn_cat Form Der
   -/
-  meta def lift_derive_syn_cat (numobjs : nat) (nummor : nat) (T : tactic unit) : tactic unit :=
+  meta def lift_derive_syn_cat {Form : Type} [Der : has_derives Form] (numobjs : nat) (nummor : nat) (T : tactic unit) : tactic unit :=
   do
-    synPoset_tactics.repeat_assume_induct (gen_nameList `φ_ numobjs),
-    synPoset_tactics.repeat_assume (gen_nameList `f_ nummor),
+    repeat_assume_induct (gen_nameList `φ_ numobjs),
+    repeat_assume (gen_nameList `f_ nummor),
     applyc `synCat.syn_hom,
-    synPoset_tactics.trace_goal "BEGIN",
+    trace_goal "BEGIN",
     trace "-- BEGIN USE TACTIC --",
     T,
     trace "-- END USE TACTIC --",
-    synPoset_tactics.trace_goal "END",
+    trace_goal "END",
     @cleanup Form Der 
 
 end synCat_tactics
--- src/semantics/syntacticPoset.lean
+++ src/semantics/syntacticPoset.lean
@@ -1,39 +1,4 @@
-import deduction.deduction category_theory.category.preorder
-
-namespace synPoset_tactics
-
-  open tactic
-  open nat
-
-  meta def trace_goal (iden : string) : tactic unit :=
-    do  
-      tactic.trace ("-- GOAL @ " ++ iden ++ " --"),
-      tactic.trace_state
-      
-  meta def repeat_assume_pair : list name → tactic (list (name × expr))
-  | [] := return []
-  | (nm::nms) :=
-    (do
-      temp_nm ← mk_fresh_name,
-      e ← intro temp_nm,
-      rest ← repeat_assume_pair nms,
-      return $ (nm,e)::rest)
-    -- <|> (return [])
-
-  meta def repeat_assume : list name → tactic unit :=
-    list.foldr (λ nm rest, intro nm >> rest) skip 
-
-  meta def repeat_assume_then_induct (T : tactic unit) (N : list name) : tactic unit :=
-    do
-      assumptionList ← repeat_assume_pair N,
-      T,
-      let cmb := λ nm e (res : tactic unit), (induction e [nm]) >> res,
-      list.foldr (function.uncurry cmb) skip assumptionList
-
-  meta def repeat_assume_induct : list name →  tactic unit :=
-    repeat_assume_then_induct skip
-
-end synPoset_tactics
+import generalTactics deduction.deduction category_theory.category.preorder
 
 namespace synPoset
 
@@ -104,8 +69,6 @@ namespace synPoset
 
   def Form_eq_order [Der : has_derives Form] : @Form_eq Form Der → @Form_eq Form Der → Prop 
     := quot.lift₂ synPoset.syn_preorder.le syn_preorder_liftable1 syn_preorder_liftable2
-
-  open synPoset_tactics
 
   instance syn_poset [Der : has_derives Form] : partial_order Form_eq :=
   { le := @Form_eq_order Form Der,
